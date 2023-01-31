576398a79c62451dff19c01869a86a297d7a82f3
christina <christinayxh@163.com>
2023-02-01 00:57:59 +0800

    css

--- src/pages/Welcome1.tsx
+++ src/pages/Welcome1.tsx
@@ -3,6 +3,10 @@ export const Welcome1: React.FC = () => {
   return (
     <div>
       <img src = {page1}/>
+      <h2 text-center>
+        会挣钱<br/>
+        还要会省钱
+      </h2>
     </div>
   )
 }
