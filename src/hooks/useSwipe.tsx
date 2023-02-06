c50c03a5a94d8dec09d179d4685e1dfd80de38a8
christina <christinayxh@163.com>
2023-02-06 22:35:54 +0800

    实现子路由跳转

---
+++ src/hooks/useSwipe.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef, useState } from "react"
+interface Config {
+    onTouchStart?: (e:TouchEvent) => void
+    onTouchMove?: (e:TouchEvent) => void
+    onTouchEnd?: (e:TouchEvent) => void
+}
+export const useSwipe = (elementRef: RefObject<HTMLElement>,config:Config) => {
+    const [direction,setDirection] = useState<''|'left'|'right'>('')
+    const x = useRef(-1) //number可以不写
+    const onTouchStart = (e:TouchEvent) => { 
+    config?.onTouchStart?.(e)
+       e.preventDefault()
+       x.current = e.touches[0].clientX
+    }
+    const onTouchMove = (e:TouchEvent) => {
+        const newX = e.touches[0].clientX
+        const d = newX - x.current
+        config?.onTouchMove?.(e)
+        if(Math.abs(d) < 3 ){
+            setDirection('')
+        }else if (d > 0 ){
+            setDirection('right')
+        }else{
+            setDirection('left')
+        }
+     }
+    const onTouchEnd = (e:TouchEvent) => { 
+        config?.onTouchEnd?.(e)
+        setDirection('')
+    } 
+    useEffect(() =>{
+        if(!elementRef.current){return}
+        elementRef.current.addEventListener('touchstart',onTouchStart)
+        elementRef.current.addEventListener('touchmove',onTouchMove)
+        elementRef.current.addEventListener('touchEnd',onTouchEnd)
+        return ()=>{
+        if(!elementRef.current){return}
+        elementRef.current.removeEventListener('touchstart',onTouchStart)
+        elementRef.current.removeEventListener('touchmove',onTouchMove)
+        elementRef.current.removeEventListener('touchEnd',onTouchEnd)
+        }
+    },[])
+    return {direction}
+}
--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,8 +1,9 @@
 import { animated, useTransition } from '@react-spring/web'
-import type { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
 import { useRef, useState } from 'react'
-import { Link, useLocation, useOutlet } from 'react-router-dom'
+import { Link, useLocation, useNavigate, useOutlet } from 'react-router-dom'
 import logo from '../assets/images/logo.svg'
+import { useSwipe } from '../hooks/useSwipe'
 
 const linkMap: Record<string, string> = {
   '/welcome/1': '/welcome/2',
@@ -10,9 +11,10 @@ const linkMap: Record<string, string> = {
   '/welcome/3': '/welcome/4',
   '/welcome/4': '/welcome/xxx',
 }
+
 export const WelcomeLayout: React.FC = () => {
   const animating = useRef(false)
-  const map = useRef<Record<string, ReactNode>>({})
+  const map = useRef<Record<string, ReactNode>>({})//hashmap类型
   const location = useLocation()
   const outlet = useOutlet()
   map.current[location.pathname] = outlet
@@ -26,16 +28,30 @@ export const WelcomeLayout: React.FC = () => {
       setExtraStyle({ position: 'absolute' })
     },
     onRest: () => {
+      animating.current = false
       setExtraStyle({ position: 'relative' })
     }
   })
+    const onSkip = ()=>{
+      localStorage.setItem('hasReadWelcomes','yes')
+    }
+    const nav = useNavigate()
+    const main = useRef<HTMLElement>(null)//html元素类型
+    const {direction} = useSwipe(main,{onTouchStart: e => e.preventDefault()})
+    useEffect(()=>{
+      if(direction === 'left'){
+        if(animating.current === true) {return}
+        animating.current = true
+        nav(linkMap[location.pathname])
+      }
+    },[direction,location.pathname,linkMap])
   return (
     <div className="bg-#5f34bf" h-screen flex flex-col items-stretch pb-16px>
       <header shrink-0 text-center pt-64px>
         <img src={logo} w-64px h-69px />
         <h1 text="#D4D4EE" text-32px>ali记账</h1>
       </header>
-      <main shrink-1 grow-1 relative >
+      <main shrink-1 grow-1 relative ref={main}>
         {transitions((style, pathname) =>
           <animated.div key={pathname} style={{ ...style, ...extraStyle }} w="100%" h="100%" p-16px flex>
             <div grow-1 bg-white flex justify-center items-center rounded-10px>
@@ -46,7 +62,7 @@ export const WelcomeLayout: React.FC = () => {
       </main>
       <footer shrink-0 text-center text-24px text-white grid grid-cols-3 grid-rows-1>
         <Link style={{ gridArea: '1 / 2 / 2 / 3' }} to={linkMap[location.pathname]}>下一页</Link>
-        <Link style={{ gridArea: '1 / 3 / 2 / 4' }} to="/welcome/xxx">跳过</Link>
+        <Link style={{ gridArea: '1 / 3 / 2 / 4' }} to="/home" onClick={onSkip}>跳过</Link>
       </footer>
     </div>
   )
