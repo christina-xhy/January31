1e679a7ad25ad3ba1ecad7a3f878cc6793b79b41
christina <christinayxh@163.com>
2023-02-10 19:15:47 +0800

    TimeRangePicker 页面设置

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,32 +1,34 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { AddItemFloatButton } from "../components/AddItmeFloatButton";
-import { TimeRangePicker } from "../components/TimeRangePicker";
+import { TimeRange, TimeRangePicker } from "../components/TimeRangePicker";
 import { Topnav } from "../components/Topnav";
 import { useTitle } from "../hooks/useTitle";
-import { ItemsList } from "./ItemsPages.tsx/ItemsList";
-import { ItemsSummary } from "./ItemsPages.tsx/ItemsSummary";
+import { ItemsList } from "./ItemsPages/ItemsList";
+import { ItemsSummary } from "./ItemsPages/ItemsSummary";
+
+
 
 interface Props{
     title?:string
 }
-
 const Div = styled.div`
-background:linear-gradient(180deg, rgba(253,164,175,1) 0%, rgba(255,230,228,1) 100%,rgba(0,0,0,1) 100%);
-
+  background:linear-gradient(20deg, rgba(253,164,175,1) 0%, rgba(255,230,228,1) 100%,
+  rgba(0,0,0,1) 100%);
 `
 
 export const ItemsPage : React.FC<Props> = (props) => {
     useTitle(props.title)
+    const [timeRange,setTimeRange] = useState<TimeRange>('thisMonth')
   return (
    <div>
      <Div>
        <Topnav />
-       <TimeRangePicker />
+       <TimeRangePicker  selected = {timeRange} onSelected={setTimeRange} />
      </Div>
        <ItemsSummary />
        <ItemsList />
        <AddItemFloatButton />
-     
    </div>
   )
 }
