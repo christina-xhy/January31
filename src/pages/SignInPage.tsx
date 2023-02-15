70c3189028a38b753eb695b4f5aa9b325adca2f6
christina <christinayxh@163.com>
2023-02-15 22:09:35 +0800

    完成登录页面的样式

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -5,10 +5,27 @@ import { TopNav } from "../components/TopNav"
 export const SignInPage : React.FC = () => {
 
   return (
-    <Gradient>
-        <TopNav title ='登陆' icon = {
-            <Icon name = 'back'></Icon>
-        }/>
-    </Gradient>
+   <div>
+         <Gradient>
+            <TopNav title ='登录' icon = { <Icon name = 'back'></Icon> }/>
+         </Gradient>
+         <div text-center pt-40px pb-16px>
+            <Icon name = 'logo' className = 'w-64px h-68px'></Icon>
+            <h1 text-28px text='#fda4af' font-bold>ali记账</h1>
+        </div>
+        <form j-form>
+            <div>
+                <span j-form-label >邮箱地址</span>
+                <input j-input-text rounded-8px type='text' placeholder="请输入邮箱，然后点击发送验证码"></input>
+            </div>
+            <div>
+                <span j-form-label>验证码</span>
+                <div flex>
+                    <input j-input-text rounded-8px type='text' placeholder='6位数字'></input>
+                    <button j-btn ml-16px>发送验证码</button>
+                </div>
+            </div>
+        </form>
+   </div>
   )
 }
