ace3fc3b3dce3758321e48e9794a566543c4678a
christina <christinayxh@163.com>
2023-02-16 01:24:09 +0800

    样式调整

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { viteMockServe } from 'vite-plugin-mock'
 import { svgsprites } from './vite_plugins/svgsprites'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command })=> ({
-  define:{
+export default defineConfig(({ command }) => ({
+  define: {
     isDev: command === 'serve'
   },
   plugins: [
     Unocss(),
     react(),
     viteMockServe(),
-    svgsprites({ noOptimizedList : ['chart','category','export','notice','logo']})
+    svgsprites({ noOptimizedList: ['chart', 'category', 'export', 'notice', 'logo'] })
   ]
 }))
